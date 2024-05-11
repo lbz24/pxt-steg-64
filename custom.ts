340af8f6c08659b090e933d6d09134f09bb2db29
lbz24 <161616366+lbz24@users.noreply.github.com>
2024-05-11 12:01:13 +0100

    Update pxt.json, custom.ts

--- custom.ts
+++ custom.ts
@@ -175,27 +175,11 @@ function findIndex(img: Images): number {
     }
 }
 
-// internal function to find enumeration type from the index
-// note: if parameter is -1 return the current image as default
-function findEnum(index: number): Images {
-    if (index === -1)
-        index = currentIndex;
-    switch (index) {
-        case 0: return Images.Heart;
-        case 1: return Images.Apple;
-        case 2: return Images.Dinosaur;
-        case 3: return Images.Pacman;
-        case 4: return Images.Ghost;
-        case 5: return Images.Alien;
-        case 6: return Images.Crown;
-        case 7: return Images.Stars;
-        case 8: return Images.Rainbow;
-        case 9: return Images.Black;
-        default: return Images.Black;
-    }
+// internal function to support showColour
+function showColourInt(pixel: number, red: number, green: number, blue: number): void {
+    display.setPixelColor(pixel, neopixel.colors(neopixel.rgb(red, green, blue)))
+    display.show()
 }
-
-//--
 
 // internal function to display an image by its index number on the Zip64 leds
 function showImageIndex(imgIndex: number): void {
@@ -207,6 +191,141 @@ function showImageIndex(imgIndex: number): void {
     currentPixel = 0;       // used for moving around the image
 }
 
+// *************************************
+// functions relating to ZIP64  buttons
+// *************************************
+
+// internal function to return array with r,g,b components from the decimal colours
+function getRGB(colour: number): number[] {
+    let r = Math.floor(colour / 256 / 256);
+    let g = Math.floor(colour / 256) % 256;
+    let b = colour % 256;
+    let rgb_array = [r, g, b];
+    return rgb_array;
+}
+
+// internal recursive function to convert decimal number to binary
+// note: returned binary number always has leading zero
+// note: tried using toString with radix 2, but reported error (expected no parameter) 
+function recursiveDecBin(dec_num: number): string {
+    if (dec_num === 0)
+        // base case: return "0" if dec_num=0 
+        return "0";
+    else {
+        // first part: recursive call with integer division of dec_num by 2
+        // second part: append remainder (gives string)
+        return (recursiveDecBin(Math.floor(dec_num / 2))
+            + (dec_num % 2));
+    }
+}
+
+// internal wrapper function for recursiveDecBin
+// note: expected num bits is 6 or 8; returned str will be 6 or 8 bit binary
+function convertDecBin(dec_num: number, bits: number): string {
+    if (bits === 6) {
+        // only convert 0..63 (to give 6 bit binary). Otherwise return 0
+        if (dec_num > 63 || dec_num < 0)
+            return "000000"
+    }
+    else if (bits === 8) {
+        // only convert 0..255 (to give 8 bit binary). Otherwise return 0
+        if (dec_num > 255 || dec_num < 0)
+            return "00000000"
+    }
+
+    // dec_num is in correct range, so call to recursive function
+    let bin = recursiveDecBin(dec_num);
+
+    // ensure returned str is 6 or 8 bit binary
+    let len = bin.length;
+    if (len > bits) {
+        // get rid of leading zero from recursive function
+        return bin.slice(1);
+    }
+    else {
+        // pad with leading zeros to get 6 bit binary
+        for (let i = 0; i < bits - len; i++)
+            bin = "0" + bin;
+        return bin;
+    }
+}
+
+// --- CONTROL GREEN BUTTONS ---
+
+// -- TOP GREEN BTN: SHOW RGB COLOUR as string on MICRO:BIT --
+function showRGBcolour(): void {
+    let colour = imagesArr[currentIndex][currentPixel];
+    let colour_array = getRGB(colour);
+    basic.showString(colour_array[0] + "," + colour_array[1] + "," + colour_array[2], scrollSpeed);
+}
+
+GAME_ZIP64.onButtonPress(GAME_ZIP64.ZIP64ButtonPins.Fire1, GAME_ZIP64.ZIP64ButtonEvents.Click, function () {
+    showRGBcolour();
+})
+
+// -- BOTTOM GREEN BTN: SHOW 2 LSBs as BINARY string on MICRO:BIT --
+function get2LeastSig(): void {
+    let colour = imagesArr[currentIndex][currentPixel];
+    let colour_array = getRGB(colour);  // rgb array: [r, g, b]
+    let result = "";
+    for (let i = 0; i < 3; i++) {
+        let str = convertDecBin(colour_array[i], 8) // convert as 8 bit binary
+        let LSBs = str.slice(-2); // get 2 least significant bits
+        result = result + LSBs + ",";
+    }
+    basic.showString(result.slice(0, -1), scrollSpeed);
+}
+
+GAME_ZIP64.onButtonPress(GAME_ZIP64.ZIP64ButtonPins.Fire2, GAME_ZIP64.ZIP64ButtonEvents.Click, function () {
+    get2LeastSig();
+})
+
+// --- CONTROL MOVEMENT ---
+
+function tempHighlightPixel(): void {
+    // get original colour of pixel
+    let colour = imagesArr[currentIndex][currentPixel];
+    let colour_array = getRGB(colour);
+
+    // change pixel to white
+    display.setPixelColor(currentPixel, neopixel.colors(NeoPixelColors.White))
+    display.show();
+    basic.pause(100);
+
+    // change back to original colour
+    display.setPixelColor(currentPixel, neopixel.rgb(colour_array[0], colour_array[1], colour_array[2]))
+    display.show();
+}
+
+// control movement around the Zip64 display
+GAME_ZIP64.onButtonPress(GAME_ZIP64.ZIP64ButtonPins.Right, GAME_ZIP64.ZIP64ButtonEvents.Click, function () {
+    currentPixel++;
+    if (currentPixel % 8 === 0)
+        currentPixel = currentPixel - 8;
+    tempHighlightPixel();
+})
+
+GAME_ZIP64.onButtonPress(GAME_ZIP64.ZIP64ButtonPins.Left, GAME_ZIP64.ZIP64ButtonEvents.Click, function () {
+    currentPixel--;
+    if (currentPixel < 0 || currentPixel % 8 === 7)
+        currentPixel = currentPixel + 8;
+    tempHighlightPixel();
+})
+
+GAME_ZIP64.onButtonPress(GAME_ZIP64.ZIP64ButtonPins.Down, GAME_ZIP64.ZIP64ButtonEvents.Click, function () {
+    currentPixel = currentPixel + 8;
+    if (currentPixel > 63)
+        currentPixel = currentPixel - 64;
+    tempHighlightPixel();
+})
+
+GAME_ZIP64.onButtonPress(GAME_ZIP64.ZIP64ButtonPins.Up, GAME_ZIP64.ZIP64ButtonEvents.Click, function () {
+    currentPixel = currentPixel - 8;
+    if (currentPixel < 0)
+        currentPixel = currentPixel + 64;
+    tempHighlightPixel();
+})
+
 
 // --
 /**
@@ -215,6 +334,31 @@ function showImageIndex(imgIndex: number): void {
 
 //% color="#B39EF3" weight=115
 namespace cryptsteg {
+
+    // SHOW COLOUR
+    /**
+     * show the given rgb colour at the specified pixel 
+     * note: show colour doesn't write this information to the underlying image
+     */
+    //% block="show colour red $red|green $green|blue $blue|at pixel $pixel"
+    //% pixel.min=0 pixel.max=63 v.defl=0
+    //% red.min=0 red.max=255 red.defl=0
+    //% green.min=0 green.max=255 green.defl=0
+    //% blue.min=0 blue.max=255 blue.defl=0
+    //% inlineInputMode=inline
+    export function showColour(pixel: number, red: number, green: number, blue: number): void {
+        showColourInt(pixel, red, green, blue);
+    }
+
+    // CLEAR DISPLAY
+    /**
+     * clearDisplay clears the image from the Zip64 leds
+     */
+    //% block
+    export function clearDisplay(): void {
+        display.clear();
+        display.show();
+    }
 
     // SHOW NEXT IMAGE
     /**
