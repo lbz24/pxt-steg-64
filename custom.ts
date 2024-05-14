94e01ac0d9b9e576af748ffd6569f27450bd7199
lbz24 <161616366+lbz24@users.noreply.github.com>
2024-05-14 19:40:35 +0100

    Update custom.ts

--- custom.ts
+++ custom.ts
@@ -499,7 +499,7 @@ namespace cryptsteg {
     //% block="show next steganographic image"
     export function showNextStegImage(): void {
         displayNextStegImage();
-        basicShow('h');      // hidden message (steganographic image)
+        basicShow('s');      // hidden message (steganographic image)
     }
 
     // SHOW ENCRYPTED IMAGE
@@ -511,7 +511,7 @@ namespace cryptsteg {
         let index = findIndex(img);
         let image = findImage(img);
         displayStegImage(steg_msgs[index], index, image, 0);
-        basicShow('h');      // hidden message (steganographic image)
+        basicShow('s');      // hidden message (steganographic image)
     }
 
     // SHOW COLOUR
